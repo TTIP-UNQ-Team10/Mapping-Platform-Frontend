90a9cc4e490192e2704bafbaac254325128d21e7
leonardo <leonardo@camba.lan>
2020-04-15 21:18:03 -0300

    First step of Poc

---
+++ mapping-platform/src/js/app.js
@@ -0,0 +1,8 @@
+// 
+// export const getMap = () => {
+//   let latlng = new google.maps.LatLng(39.305, -76.617);
+//   map = new google.maps.Map(document.getElementById('googleMap'),{
+//       center: latlng,
+//       zoom: 12
+//   });
+// };
