0286657db87282eb9e7167874e80818ad1a66ea7
leonardo <leonardo@camba.lan>
2020-05-08 23:08:09 -0300

    Added router into App

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React from 'react';
 import './App.css';
 // import Home from './pages/Home.js'
 import Login from './pages/Login.js'
+import BaseHome from './pages/BaseHome.js'
+import Hospitals from './pages/Hospitals.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 // const APP_LOGO = '/mapping-platform-logo.svg'
 
 const App = () => {
 
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/">
+            <BaseHome />
+          </Route>
+          <Route path="/hospitals">
+            <Hospitals />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
