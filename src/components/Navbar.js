db56fda6e1ca84ebce2b19b99089a7cfd5eece5a
leonardo <leonardo@camba.lan>
2020-05-08 23:07:34 -0300

    Created Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,57 @@
 import React from 'react'
 
+const APP_LOGO = '/mapping-platform-logo.svg'
 
 const userInformation = () => {
   const styles = {
-    userIcon: {
-      fontSize: 38
+    user__icon: {
+      fontSize: 26,
+      color: '#f3f3f3',
+      marginRight: 10,
+      verticalAlign: 'middle'
     },
-    userName: {
-      fontSize: 20,
-      margin: '0.3em 0.3em 0.3em 0.3em'
+    user__name: {
+      fontSize: 12,
+      color: '#f3f3f3',
+      margin: '0 auto',
+      verticalAlign: 'middle'
     }
   }
 
   return (
     <div className="col col-md-12 col-sm-4 ds-flex flex-row">
-      <i className="fa fa-user-circle" style={styles.userIcon} aria-hidden="true">
-        <span style={styles.userName}>Nombre de Usuario</span>
-      </i>
+      <i className="fa fa-user-circle" style={styles.user__icon} aria-hidden="true" />
+      <span style={styles.user__name}>Administrador</span>
     </div>
   )
 }
 
+const openSideBarButton = (
+  <a className="btn btn-sm btn-dark"
+    data-toggle="collapse" href="#sidebarMenu"
+    role="button" aria-expanded="false"
+    aria-controls="collapseExample"
+  ><i className="fa fa-bars"/></a>
+)
+
 
 const Navbar = () => {
-  // const [user] = useState()
+  const styles = {
+    image__logo: {
+      width: '6vm',
+      height: '40px',
+      filter: 'invert(1)',
+      marginLeft: 10
+    }
+  }
 
   return (
-    <nav className="navbar navbar-primary bg-primary">
-      <div className="ds-flex flex-row">
-        <h3>Mapping Platform</h3>
+    <nav className="navbar navbar-dark bg-dark mb-0">
+    <div className="ds-flex flex-row">
+      {openSideBarButton}
+        <img src={APP_LOGO} alt="mapping-platform-logo" style={styles.image__logo}/>
+      </div>
+      <div>
         {userInformation()}
       </div>
     </nav>
