fb19a8e0b1b7f2b094df06b3397b1b95f2e0227a
leonardo <leonardo@camba.lan>
2020-06-04 23:51:50 -0300

    Added logic for create, update and remove a necessity type

--- src/components/NecessityTable.js
+++ src/components/NecessityTable.js
@@ -4,23 +4,26 @@ import config from '../config.js'
 const { colors } = config
 
 const renderCategoriesModal = (necessity, idx) => {
+  const { categories } = necessity
+
   return (
     <div className="modal fade" id={`modal-${idx}`} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">{necessity.name}</h5>
+            <h5 className="modal-title" id="exampleModalLabel">Categorías de la Necesidad {necessity.name}</h5>
             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div className="modal-body">
-            {
-              necessity.categories.map(category => {
-                return (
-                  <div><span>{category}</span><br/></div>
-                )
-              })
+            { categories && categories.length > 0 ?
+                categories.map(category => {
+                  return (
+                    <div><span>{category.name}</span><br/></div>
+                  )
+                }) :
+                <span>Sin Categorías</span>
             }
           </div>
         </div>
@@ -45,7 +48,7 @@ const renderNecessityCategories = (necessity, idx) => {
       style={styles.button__show_categories}
         data-toggle="modal" data-target={`#modal-${idx}`}
       >
-        Ver Categorías
+      <span>Ver </span><i className="fa fa-eye"/>
       </button>
       {renderCategoriesModal(necessity, idx)}
     </div>
@@ -53,7 +56,7 @@ const renderNecessityCategories = (necessity, idx) => {
 }
 
 
-const NecessityTable = ({ data }) => {
+const NecessityTable = ({ data, onDeleteNecessityType, onEditNeccesityType }) => {
   const styles = {
     table__head: {
       backgroundColor: colors.navBarOptions.backgroundColor,
@@ -62,24 +65,35 @@ const NecessityTable = ({ data }) => {
     }
   }
 
+
+  const deleteNecessityType = async (necessityTypeId) => {
+    await onDeleteNecessityType(necessityTypeId)
+  }
+
+
+  const updateNecessityType = async (necessityType) => {
+    await onEditNeccesityType(necessityType.id, necessityType)
+  }
+
+
   return (
     <table className="table table-striped table-md" >
       <thead className="thead" style={styles.table__head}>
-        <th style={{width: '90%'}}>Necesidad</th>
+        <th style={{width: '65%'}}>Tipo de Necesidad</th>
         <th style={{width: '10%'}}>Categorías</th>
         <th style={{width: '10%'}}></th>
       </thead>
       <tbody>
         {
-          data.map(necessity => {
-            const idx = data.indexOf(necessity).toString()
+          data.map(necessityType => {
+            const idx = data.indexOf(necessityType).toString()
             return (
-              <tr id={`necessity:${necessity.id}`}>
-                <th>{necessity.name}</th>
-                <th>{renderNecessityCategories(necessity, idx)}</th>
+              <tr id={`necessityType:${necessityType.id}`}>
+                <th>{necessityType.name}</th>
+                <th>{renderNecessityCategories(necessityType, idx)}</th>
                 <th className="row">
-                  <i className="fa fa-edit edit__icon" title="Editar"/>
-                  <i className="fa fa-times times__icon" title="Eliminar"/>
+                  <i className="fa fa-edit edit__icon" title="Editar" onClick={updateNecessityType(necessityType)}/>
+                  <i className="fa fa-times times__icon" title="Eliminar" onClick={deleteNecessityType(necessityType.id)}/>
                 </th>
               </tr>
             )
